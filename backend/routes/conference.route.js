3bbd644c56e8f395c9c5ad4189cadd9bafa6c67b
Avishka Shyaman <avishkashyaman@gmail.com>
2021-06-30 04:16:01 +0530

    fixed user end point bug

--- backend/routes/conference.route.js
+++ backend/routes/conference.route.js
@@ -14,9 +14,9 @@ const {
 } = require("../middleware/auth.middleware");
 
 router.get("/", get);
+router.get('/pending', authorize, roleAuthorization("editor", "admin"), getPending);
 router.get("/:id", getConference);
 router.post("/", authorize, roleAuthorization("editor", "admin"), add);
 router.put("/:id", authorize, roleAuthorization("editor", "admin"), update);
-router.get('/pending', authorize, roleAuthorization("editor", "admin"), getPending);
 router.put('/confirmation', authorize, roleAuthorization("editor", "admin"), conferenceConfirmation);
 module.exports = router;
