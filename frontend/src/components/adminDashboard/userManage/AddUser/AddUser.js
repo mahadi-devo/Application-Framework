2d47c3b106d6ac754902dc6424b574885d1ee385
Avishka Shyaman <avishkashyaman@gmail.com>
2021-06-29 20:41:00 +0530

    modified AddUser featuer in adminDashboard component

--- frontend/src/components/adminDashboard/userManage/AddUser/AddUser.js
+++ frontend/src/components/adminDashboard/userManage/AddUser/AddUser.js
@@ -14,7 +14,8 @@ import Container from '@material-ui/core/Container';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Radio } from '@material-ui/core';
-import { Link,useHistory  } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import axios from 'axios';
 
 toast.configure();
 
@@ -50,36 +51,56 @@ const AddUser = (props) => {
     password: '',
     phone: '',
     role: 'admin',
+    sendemail: false,
   });
 
-  const { name, email, password, phone, role, username } = user;
+  const { name, email, password, phone, role, username, sendemail } = user;
 
   const [, setCheck] = useState(false);
 
   const onChange = (e) => {
+    console.log(e.target.name,' ', e.target.value);
     setUser({ ...user, [e.target.name]: e.target.value });
     setCheck(e.target.checked);
-  };
-
-  const getFile = (FileData) => {
-    setUser({ ...user, resource: FileData });
   };
 
   const onsubmit = async (e) => {
     e.preventDefault();
 
-    if (name === '' || email === '' || password === '' || phone === '') {
+    if (name === '' || email === '' || phone === '') {
       toast('Fields can not be empty', { type: 'error' });
     } else {
-      
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      };
+
+      axios
+        .post(
+          'http://localhost:5000/api/v1/user',
+          { name, email, phone, role, password: user.name },
+          config
+        )
+        .then((res) => {
+          console.log(res.data.user);
+          toast('User successfully added', { type: 'success' });
+
+          if(sendemail === 'true') {
+            console.log('sendemail');
+          }
+          history.goBack();
+        })
+        .catch((error) => {});
     }
 
     setUser({
       name: '',
+      username: '',
       email: '',
       password: '',
       phone: '',
-      role: 'buyer',
+      role: '',
     });
   };
 
@@ -149,7 +170,7 @@ const AddUser = (props) => {
                 onChange={onChange}
               />
             </Grid>
-            <Grid item md={12}>
+            <Grid item md={6}>
               <Typography
                 style={{ marginTop: '20px' }}
                 variant="subtitle2"
@@ -165,22 +186,50 @@ const AddUser = (props) => {
                 style={{ marginLeft: '20px' }}
               >
                 <FormControlLabel
-                  value="researcher"
+                  value="admin"
                   control={<Radio color="primary" />}
                   label="Admin"
                 />
                 <FormControlLabel
-                  value="workshopConductor"
+                  value="editor"
                   control={<Radio color="primary" />}
                   label="Editor"
                 />
                 <FormControlLabel
-                  value="attendee"
+                  value="reviewer"
                   control={<Radio color="primary" />}
                   label="Reviewer"
                 />
               </RadioGroup>
             </Grid>
+            <Grid item md={6}>
+              <Typography
+                style={{ marginTop: '20px' }}
+                variant="subtitle2"
+                gutterBottom
+              >
+                Send Email to the User
+              </Typography>
+              <RadioGroup
+                aria-label="customerType"
+                name="sendemail"
+                value={sendemail}
+                onChange={onChange}
+                style={{ marginLeft: '20px' }}
+              >
+                <FormControlLabel
+                  value='true'
+                  control={<Radio color="primary" />}
+                  label="Yes"
+                />
+                <FormControlLabel
+                  value={'false'}
+                  control={<Radio color="primary" />}
+                  label="No"
+                />
+              </RadioGroup>
+            </Grid>
+          
           </Grid>
           <Grid container spacing={2}>
             <Grid item item xs={6}>
